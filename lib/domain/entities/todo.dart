60e6023b7f63d091503ef6c6e3a8a981188fd236
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-10 19:56:21 -0300

    feat: add entity domain to do

---
+++ lib/domain/entities/todo.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class ToDo extends Equatable {
+  final String id;
+  final String title;
+  final bool isDone;
+
+  const ToDo({
+    required this.id,
+    required this.title,
+    required this.isDone,
+  })
+
+  @override
+  List<Object> get props => [id,title, isDone];
+}
+
+// A entidade (Entity) na camada Domain representa um modelo de negócio puro, sem dependência de banco, framework ou Firebase.
+// 'E a entidade do domínio, limpa, usada por Use Cases.
+
+// extends [Equatable]	Faz com que objetos ToDo possam ser comparados por valor, e não por ponteiro
+// get props => [...]	Define os atributos usados na comparação (==) e hashCode automaticamente
+
