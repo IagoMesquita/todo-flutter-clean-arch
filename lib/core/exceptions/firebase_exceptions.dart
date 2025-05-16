53b6fd05241a591a4dc58312c3b86a805a3d157c
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-15 22:25:50 -0300

    feat: class exceptions firebase

---
+++ lib/core/exceptions/firebase_exceptions.dart
@@ -0,0 +1,44 @@
+class FirestoreException implements Exception {
+  final String message;
+  final String? code;
+  final StackTrace? stackTrace;
+
+  const FirestoreException(
+      {required this.message,  this.code, this.stackTrace});
+
+  @override
+  String toString() => 'FirestoreException($code): $message\n$stackTrace';
+}
+
+class GetToDosException extends FirestoreException {
+  const GetToDosException(String message, String? code, StackTrace? stackTrace)
+      : super(
+          message: message,
+          code: code,
+          stackTrace: stackTrace,
+        );
+}
+
+class AddToDoException extends FirestoreException {
+  AddToDoException(
+    String message,
+    String? code,
+    StackTrace? stackTrace,
+  ) : super(
+          message: message,
+          code: code,
+          stackTrace: stackTrace,
+        );
+}
+
+class DeleteToDoException extends FirestoreException {
+  DeleteToDoException(
+    String message,
+    String? code,
+    StackTrace? stackTrace,
+  ) : super(
+          message: message,
+          code: code,
+          stackTrace: stackTrace,
+        );
+}
