728d892e4eb570ca7db4080ddc596a4f69031f93
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-17 12:11:47 -0300

    feat: add BlocBuilder and ListView

--- lib/presentation/pages/todo_page.dart
+++ lib/presentation/pages/todo_page.dart
@@ -27,7 +27,58 @@ class ToDoPage extends StatelessWidget {
         deleteToDo: DeleteToDo(repository),
       )..fetchToDos(),
       child: Scaffold(
-        
+        appBar: AppBar(
+          title: const Text('Sua lista de tarefas'),
+        ),
+        body: BlocBuilder<ToDoCubit, ToDoState>(
+          builder: (context, state) {
+            if (state is ToDoLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is ToDoError) {
+              return Center(
+                child: Text(state.message),
+              );
+            } else if (state is ToDoLoaded) {
+              if (state.todos.isEmpty) {
+                return const Center(
+                  child: Center(child: Text('Nenhum tarefa listada !')),
+                );
+              }
+
+              return ListView.builder(
+                itemCount: state.todos.length,
+                itemBuilder: (context, index) {
+                  final todo = state.todos[index];
+
+                  return ListTile(
+                    leading: Checkbox(
+                      value: todo.isDone,
+                      onChanged: (_) {
+                        context.read<ToDoCubit>().toggleToDoCompletion(todo.id);
+                      },
+                    ),
+                    title: Text(
+                      todo.title,
+                      style: TextStyle(
+                        decoration:
+                            todo.isDone ? TextDecoration.lineThrough : null,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        context.read<ToDoCubit>().removeToDo(todo.id);
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+            return const SizedBox.shrink(); // estado inicial
+          },
+        ),
       ),
     );
   }
