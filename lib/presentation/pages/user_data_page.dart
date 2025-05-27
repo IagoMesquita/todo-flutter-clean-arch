392d50441fa66c3cc2593d9ec9ade6bb75292d6a
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-27 20:48:17 -0300

    feat: add page user data

---
+++ lib/presentation/pages/user_data_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class UserDataPage extends StatefulWidget {
+  const UserDataPage({super.key});
+
+  @override
+  State<UserDataPage> createState() => _UserDataPageState();
+}
+
+class _UserDataPageState extends State<UserDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+} 
