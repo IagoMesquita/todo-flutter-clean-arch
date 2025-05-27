704a0265213f39833dc8a13d92e70f883918d8b0
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-27 20:47:06 -0300

    feature: adds HomePage for page navigation

---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:todo_clean_arch/presentation/pages/perfil_page.dart';
+import 'package:todo_clean_arch/presentation/pages/todo_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [const ToDoPage(), const PerfilPage()];
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (value) => setState(() => _currentIndex = value),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Perfil')
+        ],
+      ),
+    );
+  }
+}
