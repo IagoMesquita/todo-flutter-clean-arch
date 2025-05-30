adfc1cfcb5ef0d6d7ef52384f7b9e61b096701f2
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-05-30 09:14:49 -0300

    feature: add user mapper

---
+++ lib/features/user/extensions/user_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:todo_clean_arch/features/user/data/models/user_model.dart';
+import 'package:todo_clean_arch/features/user/domain/entities/user.dart';
+
+extension UserModelMappar on UserModel {
+  User toEntity() {
+    return User(
+      uid: uid,
+      phoneNumber: phoneNumber,
+      name: name,
+      email: email,
+    );
+  }
+}
+
+extension UserEntity on User {
+  UserModel toMoldel() {
+    return UserModel(
+      uid: uid,
+      phoneNumber: phoneNumber,
+      name: name,
+      email: email,
+    );
+  }
+}
