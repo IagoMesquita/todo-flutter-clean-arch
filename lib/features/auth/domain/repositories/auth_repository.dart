3ddbf11c85468534176f5b8aad164c0d390b53e9
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-06-03 08:19:41 -0300

    feat: implements auth repository

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,23 @@
+import 'package:todo_clean_arch/features/auth/domain/entities/auth.dart';
+
+abstract class AuthRepository {
+  /// Retorna usuario autenticado de existir.
+  Future<Auth?> getCurrentUser();
+
+  /// Envia o codigo de verificacao (OTP) para o numero de telefone.
+  Future<void> sendPhoneCode({
+    required String phoneNumer,
+    required Function(String verificationId) codeSent,
+    required Function(String message) onError,
+  });
+
+  /// Verifica o codigo recebido (OTP) e autentoca o usuario.
+  Future<Auth> verifyCode({
+    required String verificationId,
+    required String smsCode,
+  });
+
+  /// Logout do usuario atual.
+  Future<void> logout();
+}
+
