b1e9562f91f2739bcb2da1d9ab857fadb1f9a759
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-06-04 07:57:29 -0300

    feat: implelements repository auth with firebase auth

--- lib/features/auth/data/repositories/auth_repostory_impl.dart
+++ lib/features/auth/data/repositories/auth_repostory_impl.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_clean_arch/core/exceptions/firebase_exceptions.dart';
 import 'package:todo_clean_arch/features/auth/data/models/auth_model.dart';
 import 'package:todo_clean_arch/features/auth/domain/entities/auth.dart';
 import 'package:todo_clean_arch/features/auth/domain/repositories/auth_repository.dart';
@@ -8,34 +9,89 @@ class AuthRepostoryImpl extends AuthRepository {
   final FirebaseAuth _firebaseAuth;
 
   AuthRepostoryImpl(this._firebaseAuth);
-  
+
   @override
   Future<Auth?> getCurrentUser() async {
-    final user = _firebaseAuth.currentUser;
+    try {
+      final user = _firebaseAuth.currentUser;
 
-    if(user != null && user.phoneNumber != null) {
-      return AuthModel.fromFirebaseUser(user).toEntity();
+      if (user != null && user.phoneNumber != null) {
+        return AuthModel.fromFirebaseUser(user).toEntity();
+      }
+      return null;
+    } catch (e, StackTrace) {
+      throw FirestoreException(
+        message: 'Erro ao buscar um usuário',
+        stackTrace: StackTrace,
+        operation: 'getCurrentUser',
+      );
     }
-    return null;
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> sendPhoneCode({
+    required String phoneNumber,
+    required Function(String verificationId) codeSent,
+    required Function(String message) onError,
+  }) async {
+    await _firebaseAuth.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      timeout: const Duration(seconds: 60),
+      verificationFailed: (FirebaseAuthException e) {
+        onError(e.message ?? 'Erro Desconhecido');
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        codeSent(verificationId);
+      },
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        // Autenticação automática (Android): não usaremos nesse fluxo
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        // Pode ser usado se quiser tratar tempo de expiração
+      },
+    );
   }
 
   @override
-  Future<void> sendPhoneCode({required String phoneNumer, required Function(String verificationId) codeSent, required Function(String message) onError}) {
-    // TODO: implement sendPhoneCode
-    throw UnimplementedError();
+  Future<Auth> verifyCode(
+      {required String verificationId, required String smsCode}) async {
+    try {
+      final credential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: smsCode);
+
+      final result = await _firebaseAuth.signInWithCredential(credential);
+      final user = result.user;
+
+      if (user != null && user.phoneNumber != null) {
+        return AuthModel.fromFirebaseUser(user).toEntity();
+      }
+
+      throw FirestoreException(
+        message: 'Usuário inválido ou sem número de telefone.',
+        code: 'invalid-user',
+        operation: 'verifyCode',
+        stackTrace: StackTrace.current,
+      );
+    } on FirebaseAuthException catch (e, StackTrace) {
+      throw FirestoreException(
+        message: e.message ?? 'Erro ao verificar código',
+        code: e.code,
+        operation: 'verifyCode',
+        stackTrace: StackTrace,
+      );
+    }
   }
 
   @override
-  Future<Auth> verifyCode({required String verificationId, required String smsCode}) {
-    // TODO: implement verifyCode
-    throw UnimplementedError();
+  Future<void> logout() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e, StackTrace) {
+      throw FirestoreException(
+        message: 'Erro ao deslogar usuário',
+        stackTrace: StackTrace,
+        operation: 'logout',
+      );
+    }
   }
-
-
 }
