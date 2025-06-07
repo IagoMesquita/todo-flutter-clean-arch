f374ba7024fc49739dd1de8fa6ec37591d2ed6a7
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-06-07 16:20:42 -0300

    feat: add Provider Auth Cubit in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'package:todo_clean_arch/features/todo/presentation/pages/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_clean_arch/features/auth/data/repositories/auth_repostory_impl.dart';
+import 'package:todo_clean_arch/features/auth/domain/repositories/auth_repository.dart';
+import 'package:todo_clean_arch/features/auth/domain/usecases/get_current_user.dart';
+import 'package:todo_clean_arch/features/auth/domain/usecases/logout.dart';
+import 'package:todo_clean_arch/features/auth/domain/usecases/send_phone_code.dart';
+import 'package:todo_clean_arch/features/auth/domain/usecases/verify_code.dart';
+import 'package:todo_clean_arch/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:todo_clean_arch/features/auth/presentation/pages/auth_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(
-    const MyApp(),
-  );
+  final authRepository = AuthRepositoryImpl(FirebaseAuth.instance);
+
+  runApp(MyApp(authRepository: authRepository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AuthRepository authRepository;
+
+  const MyApp({super.key, required this.authRepository});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (_) => AuthCubit(
+        getCurrentUserUseCase: GetCurrentUserUseCase(authRepository),
+        sendPhoneCodeUseCase: SendPhoneCodeUseCase(authRepository),
+        verifyCodeUseCase: VerifyCodeUseCase(authRepository),
+        logoutUseCase: LogoutUseCase(authRepository),
+      )..checkAuhStatus(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const AuthPage(),
       ),
-      home: const HomePage(),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
