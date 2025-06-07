a59726a95c9e754ac8b8561656e6a728c168e013
Iago Mesquita <iagomesquitaalves@gmail.com>
2025-06-07 16:21:32 -0300

    fix: add +55 in phoneNumber

--- lib/features/auth/domain/usecases/send_phone_code.dart
+++ lib/features/auth/domain/usecases/send_phone_code.dart
@@ -5,12 +5,14 @@ class SendPhoneCodeUseCase {
 
   SendPhoneCodeUseCase(this.authRepository);
 
+  final _codeBR = '+55';
+
   Future<void> call(
       {required String phoneNumber,
       required Function(String verificationId) codeSent,
       required Function(String message) onError}) async {
     return await authRepository.sendPhoneCode(
-      phoneNumber: phoneNumber,
+      phoneNumber: '$_codeBR$phoneNumber',
       codeSent: codeSent,
       onError: onError,
     );
